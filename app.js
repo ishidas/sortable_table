3b2694fe65bd1e451e4a9e42a112243aaf2cb277
sawakoishida <sawako.ishida@tangocard.com>
2017-11-07 22:38:48 -0800

    added a thead and row

--- app.js
+++ app.js
@@ -8,7 +8,25 @@ var data = [
   {'country': 'the Great Britain', 'capital': 'London', 'population': 65640000 },
 ];
 
+//table container
+var tableContainerElement = document.querySelector('.tableContainer');
+//table
+var tableElement = document.createElement('table');
+var header = tableElement.createTHead(0);
+var newRow = header.insertRow(0);
 
-var bodyElement = document.querySelector('.tablecContainer');
 
-bodyElement.create
+for (var key in data[0]) {
+  var newCell = newRow.insertCell();
+  var newText = document.createTextNode(key + "\n");
+  newCell.appendChild(newText);
+}
+
+data.forEach((dataSet) => {
+
+});
+
+
+
+//table appended to container
+tableContainerElement.appendChild(tableElement);
