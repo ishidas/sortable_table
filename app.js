9ac1b12d0f566f553ff5c64a102f102f2bbb490b
sawakoishida <sawako.ishida@tangocard.com>
2017-11-07 22:02:51 -0800

    added base html and some data

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var data = [
+  {'country': 'Japan', 'capital': 'Tokyo', 'population': 127000000 },
+  {'country': 'the United States of America', 'capital': 'Washington D.C.', 'population': 323100000 },
+  {'country': 'Italy', 'capital': 'Rome', 'population': 60600000 },
+  {'country': 'France', 'capital': 'Paris', 'population': 66900000 },
+  {'country': 'the Great Britain', 'capital': 'London', 'population': 65640000 },
+];
+
+
+var bodyElement = document.querySelector('.tablecContainer');
+
+bodyElement.create
